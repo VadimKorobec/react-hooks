6b1919303180bda86b05cf3144fb8eef4d8b90d2
VadimKorobec <_s_e_t__@ukr.net>
2023-06-01 16:43:30 +0300

    Create UserMenu.jsx

---
+++ src/components/UserMenu.jsx
@@ -0,0 +1,15 @@
+import { useUser } from './userContext';
+
+export const UserMenu = () => {
+  const { isLoggedIn, username, logIn, logOut } = useUser();
+  return (
+    <div>
+      {isLoggedIn && <p>{username}</p>}
+      {isLoggedIn ? (
+        <button onClick={logOut}>Log out</button>
+      ) : (
+        <button onClick={logIn}>Log In</button>
+      )}
+    </div>
+  );
+};
