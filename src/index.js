87bddd3d2e44ffa2527f89b53639b2d597d5fa49
VadimKorobec <_s_e_t__@ukr.net>
2023-06-01 16:43:26 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
+import { UserContext } from 'components/userContext';
+import { UserProvider } from 'components/userContext';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <UserContext.Provider value={{ username: 'Mango' }}>
+      <UserProvider>
+        <App />
+      </UserProvider>
+    </UserContext.Provider>
   </React.StrictMode>
 );
