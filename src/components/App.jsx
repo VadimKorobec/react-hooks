91826d9940d674b5200426863102a3e84613e86a
VadimKorobec <_s_e_t__@ukr.net>
2023-06-02 14:25:34 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,24 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { UserMenu } from './UserMenu';
+import { Player } from './Player';
 
 export const App = () => {
   const [firsVtvalue, setFirstValue] = useState(0);
   const [secondValue, setSecondValue] = useState(0);
+  const [btnValue, setBtnvalue] = useState(0);
+  const valueRef = useRef(0);
 
   useEffect(() => {
     console.log(firsVtvalue + secondValue);
   }, [firsVtvalue, secondValue]);
+
+  useEffect(() => {
+    console.log(valueRef.current);
+  });
+
+  const handleClick = () => {
+    valueRef.current += 1;
+  };
 
   return (
     <div>
@@ -18,6 +29,8 @@ export const App = () => {
         Second:{secondValue}
       </button>
       <UserMenu />
+      <button onClick={handleClick}>Button with ref</button>
+      <Player sourse="http://media.w3.org/2010/05/sintel/trailer.mp4" />
     </div>
   );
 };
